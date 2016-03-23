d638166ccd213362d8d35295a90f3260ab952cc4
agalakov <agalakov@devexperts.com>
2016-03-23 12:47:54 +0300

    fix rendering

--- index.js
+++ index.js
@@ -9,36 +9,33 @@ module.exports = function ReactPageLoader(source) {
 		this.cacheable();
 	}
 	var done = this.async();
-	
+
 	var query = utils.parseQuery(this.query);
 	var target = query.target;
-	var isStatic = query.isStatic;
 
-	if (isStatic) {
+	if (!target) {
+		done(new Error('No target to render to'));
+	} else {
 		source += `
 			(function() {
-				var RDS = require('react-dom/server');
 				var React = require('react');
-				exports.default = RDS.renderToStaticMarkup(React.createElement(exports.default));
-			})();
-		`;
-		done(null, source);
-	} else {
-		if (!target) {
-			done(new Error('No target to render to'));
-		} else {
-			source += `
-				(function() {
+				var canUseDom = !!(
+					typeof window !== 'undefined' &&
+					window.document && window.document.createElement
+				);
+				if (canUseDom) {
 					var ReactDOM = require('react-dom');
-					var React = require('react');
 					ReactDOM.render(
 						React.createElement(exports.default),
 						document.querySelector(${JSON.stringify(target)})
 					);
-				})();
-			`;
-		}
-		
-		done(null, source);
+				} else {
+					var RDS = require('react-dom/server');
+					exports.default = RDS.renderToStaticMarkup(React.createElement(exports.default));
+				}
+			})();
+		`;
 	}
+
+	done(null, source);
 };
