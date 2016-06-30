f9ff08bf68387d7319f88ab798fd3386e1666a76
Kirill Agalakov <agalakov@devexperts.com>
2016-06-30 12:14:32 +0300

    fix target check

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ module.exports = function ReactPageLoader(source) {
 	if (this.cacheable) {
 		this.cacheable();
 	}
-	var done = this.async();
 
 	var query = utils.parseQuery(this.query);
 	var target = query.target;
-	var result = source;
 
 	var targetScript = target ?
 		`var target = document.querySelector(${JSON.stringify(target)});` :
@@ -22,30 +20,25 @@ module.exports = function ReactPageLoader(source) {
 		target.id = ${defaultId};
 		document.body.appendChild(target);`;
 
-	if (!target) {
-		done(new Error('No target to render to'));
-	} else {
-		result += `
-			(function() {
-				var React = require('react');
-				var canUseDom = !!(
-					typeof window !== 'undefined' &&
-					window.document && window.document.createElement
+	//noinspection JSConstructorReturnsPrimitive
+	return source + `
+		(function() {
+			var React = require('react');
+			var canUseDom = !!(
+				typeof window !== 'undefined' &&
+				window.document && window.document.createElement
+			);
+			${targetScript}
+			if (canUseDom) {
+				var ReactDOM = require('react-dom');
+				ReactDOM.render(
+					React.createElement(exports.default),
+					target
 				);
-				${targetScript}
-				if (canUseDom) {
-					var ReactDOM = require('react-dom');
-					ReactDOM.render(
-						React.createElement(exports.default),
-						target
-					);
-				} else {
-					var RDS = require('react-dom/server');
-					exports.default = RDS.renderToStaticMarkup(React.createElement(exports.default));
-				}
-			})();
-		`;
-	}
-
-	done(null, result);
+			} else {
+				var RDS = require('react-dom/server');
+				exports.default = RDS.renderToStaticMarkup(React.createElement(exports.default));
+			}
+		})();
+	`;
 };
